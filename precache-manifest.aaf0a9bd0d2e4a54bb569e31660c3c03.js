452e4bca00f330206e3ca760248b602052073e25
Sahil213 <sahidesai213@gmail.com>
2020-05-18 13:42:23 +0530

    Updates

---
+++ precache-manifest.aaf0a9bd0d2e4a54bb569e31660c3c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755ce26956243b3f91a05083c05ec1e0",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "913cfba8001a5a944a01",
+    "url": "/react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ec54e6ebc19c4243b292",
+    "url": "/react/static/js/2.e8b3108e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.e8b3108e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913cfba8001a5a944a01",
+    "url": "/react/static/js/main.ccbfacc6.chunk.js"
+  },
+  {
+    "revision": "18304c74a1065749285e",
+    "url": "/react/static/js/runtime-main.36a0aad7.js"
+  }
+]);
